1ab929f140d1f2ccd7e301288765c2a8dcfb7f38
Kamil Oratyński <kamil.oratynski@gmail.com>
2020-03-22 17:20:48 +0100

    - add 1000 example reservation

--- src/main/java/com/space/flightmanagement/model/Reservation.java
+++ src/main/java/com/space/flightmanagement/model/Reservation.java
@@ -1,10 +1,12 @@
 package com.space.flightmanagement.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "reservation")
 public class Reservation {
